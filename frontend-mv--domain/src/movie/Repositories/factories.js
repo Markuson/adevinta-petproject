7f1833ce9796c6aa5a67569f55ff5fe592ff5f44
Marc Usón <el.marcuson@gmail.com>
2019-06-17 22:53:00 +0200

    first commit from matebook

--- frontend-mv--domain/src/movie/Repositories/factories.js
+++ frontend-mv--domain/src/movie/Repositories/factories.js
@@ -1,10 +1,12 @@
 import FetcherFactories from '../../Fetcher/factories'
 import HTTPMovieRepository from './HTTPMovieRepository'
+import MovieValueObjectsFactories from '../ValueObjects/factories'
 
 export default class MoviesRepositoriesFactories {
-    static httpMovieRepository = ({ config }) => 
+    static httpMovieRepository = ({ config }) =>
     new HTTPMovieRepository ({
         fetcher: FetcherFactories.httpFetcher(),
+        movieListValueObjectFactory: MovieValueObjectsFactories.movieListValueObject,
         config
     })
 }
